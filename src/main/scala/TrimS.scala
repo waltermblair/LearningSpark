ac2bc8c9d349b08570d17686fcb924fe6d902148
walter <waltermblair@gmail.com>
2017-03-15 20:53:11 -0400

    using bash script to first convert fastq to csv

--- src/main/scala/TrimS.scala
+++ src/main/scala/TrimS.scala
@@ -5,12 +5,12 @@
 import org.apache.commons.math3.analysis.function.Max
 import org.apache.spark.sql.SparkSession
 
-import scala.collection.mutable.ArrayBuffer
-import scala.io.Source
+//import scala.collection.mutable.ArrayBuffer
+//import scala.io.Source
 
 object TrimS {
   // case class must be defined outside of scope
-  case class Read(name1: String, seq: String, name2: String, phred: String)
+//  case class Read(name1: String, seq: String, name2: String, phred: String)
 
   def main(args: Array[String]) {
     // Create SparkSession https://databricks.com/blog/2016/08/15/how-to-use-sparksession-in-apache-spark-2-0.html
@@ -23,31 +23,44 @@ object TrimS {
 
     import spark.implicits._
 
-    //http://alvinalexander.com/scala/scala-mutable-arrays-adding-elements-to-arrays
-    var data = ArrayBuffer[Read]()
+    // commented out manual parser from fastq
+    // current version uses a bash script to convert fastq to csv
+    // and then loads csv directly
+    val filepath = "/home/walter/lab/data/fastq.csv"
 
-    //http://stackoverflow.com/questions/30863747/reading-a-text-file-in-scala-one-line-after-the-other-not-iterated
-    for(line <- Source.fromFile("SRAdump.txt").getLines().grouped(4)) {
-      //noinspection ZeroIndexToHead
-      data += Read(line(0), line(1), line(2), line(3))
-    }
+    val ds = spark.read
+        .option("inferSchema", true)
+        .option("header", true)
+        .csv(filepath)
+        .toDF("name1", "seq", "name2", "phred")
 
-    // converting mutable ArrayBuffer to RDD, then to DF, then to DS
-    val ds = spark.sparkContext.makeRDD(data).toDF().as[Read]
     //// Find index of the bad phred score and trim with that num
     //// http://stackoverflow.com/questions/15259250/in-scala-how-to-get-a-slice-of-a-list-from-nth-element-to-the-end-of-the-list-w
     val badScoresArray = Array('!', '\"', '#', '$', '%', '&')
+
+    //http://alvinalexander.com/scala/scala-mutable-arrays-adding-elements-to-arrays
+//    var data = ArrayBuffer[Read]()
+
+    //http://stackoverflow.com/questions/30863747/reading-a-text-file-in-scala-one-line-after-the-other-not-iterated
+//    for(line <- Source.fromFile("SRAdump.txt").getLines().grouped(4)) {
+//      //noinspection ZeroIndexToHead
+//      data += Read(line(0), line(1), line(2), line(3))
+//    }
+
+    // converting mutable ArrayBuffer to RDD, then to DF, then to DS
+//    val ds = spark.sparkContext.makeRDD(data).toDF().as[Read]
+
     // Does using regex condition perform better?
-    //val badScoresRegex = """[!,",#,%,&]""".r
+    val badScoresRegex = """[!,",#,%,&]""".r
     ds
-      .select("seq", "phred")
+      .select($"seq", $"phred")
       //.map(row => if(badScoresRegex.findFirstIn(row.getString(1)) != None) leadingTrim(row.getString(0), row.getString(1), badScoresArray) else row.getString(0))
       .map(row => (row.getString(0), leadingTrim(row.getString(0), row.getString(1), badScoresArray), trailingTrim(row.getString(0), row.getString(1), badScoresArray)))
       .select($"_1".alias("original"), $"_2".alias("leading trim"), $"_3".alias("trailing trim"))
       .show(100, false)
 
     // write out to parquet (can send straight to next step)
-    //ds.write.parquet("trimmedParquet")
+    ds.write.parquet("outputFile/trimmedParquet")
 
     // configure spark-csv to write out to csv http://stackoverflow.com/questions/31937958/how-to-export-data-from-spark-sql-to-csv
 
